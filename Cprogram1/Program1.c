c3096b7f173128009b333f4bbfc58c2f81ed046b
shaddev <shadahmed2015@gmail.com>
2023-02-11 21:56:49 -0600

    Initial commit

---
+++ Cprogram1/Program1.c
@@ -0,0 +1,8 @@
+/*
+ * Program1.c
+ *
+ *  Created on: Feb 11, 2023
+ *      Author: s326s742
+ */
+
+
---
+++ Cprogram2/Program2.c
@@ -0,0 +1,8 @@
+/*
+ * Program2.c
+ *
+ *  Created on: Feb 11, 2023
+ *      Author: Shad Ahmed
+ */
+
+
